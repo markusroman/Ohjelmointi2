087f2c87f681bf9fd3b5bc310b7298c8c7abbe59
Markus Röman <markus.roman@tuni.fi>
2019-08-30 01:15:17 +0300

    Timantti tulostuu

--- student/01/diamond/main.cpp
+++ student/01/diamond/main.cpp
@@ -4,6 +4,13 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello World!" << endl;
+    cout << "      *      " << endl;
+    cout << "    *   *    " << endl;
+    cout << "  *       *  " << endl;
+    cout << "*           *" << endl;
+    cout << "  *       *  " << endl;
+    cout << "    *   *    " << endl;
+    cout << "      *      " << endl;
+
     return 0;
 }
