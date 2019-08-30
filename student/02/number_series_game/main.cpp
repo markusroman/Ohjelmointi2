36352dc168c84e8af572e09122faa224633ee6d8
Markus Röman <markus.roman@tuni.fi>
2019-08-30 04:08:29 +0300

    Valmis zip boing -peli

--- student/02/number_series_game/main.cpp
+++ student/02/number_series_game/main.cpp
@@ -8,7 +8,11 @@ int main()
     std::cin >> amount;
     int current = 1;
     while (current < amount + 1) {
-        if (current % 3 == 0) {
+        if (current % 3 == 0 and current % 7 == 0) {
+            std::cout << "zip boing" << std::endl;
+        } else if (current % 7 == 0) {
+            std::cout << "boing" << std::endl;
+        } else if (current % 3 == 0) {
             std::cout << "zip" << std::endl;
         } else {
             std::cout << current << std::endl;
