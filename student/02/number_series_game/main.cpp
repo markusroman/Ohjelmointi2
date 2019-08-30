6dca98555c036cd42f6e2e80343ebd39874e2d95
Markus Röman <markus.roman@tuni.fi>
2019-08-30 03:57:11 +0300

    Ohjelma tulostaa annetun määrän lukuja

--- student/02/number_series_game/main.cpp
+++ student/02/number_series_game/main.cpp
@@ -4,4 +4,11 @@
 int main()
 {
     std::cout << "How many numbers would you like to have? ";
+    int amount = 0;
+    std::cin >> amount;
+    int current = 1;
+    while (current < amount + 1) {
+        std::cout << current << std::endl;
+        current++;
+    }
 }
