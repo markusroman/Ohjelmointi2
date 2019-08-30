b42815e32acabefbae3a7079d4a823f8400cd6e3
Markus Röman <markus.roman@tuni.fi>
2019-08-30 03:11:51 +0300

    Toimiva lämpötilamuunnin

--- student/02/temperature/main.cpp
+++ student/02/temperature/main.cpp
@@ -5,7 +5,17 @@ using namespace std;
 int main()
 {
     cout << "Enter a temperature: ";
+    int temp = 0;
+    cin >> temp;
 
+    double f = 0.0;
+    double c = 0.0;
+
+    f = temp * 1.8 + 32;
+    c = (temp - 32) / 1.8;
+
+    cout << temp << " degrees in Celsius is " << f << " degrees Fahrenheit" << endl;
+    cout << temp << " degrees in Fahrenheit is " << c << " degrees Celsius" << endl;
     // Write your code here
 
     return 0;
