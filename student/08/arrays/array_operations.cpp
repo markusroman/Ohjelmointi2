64d9eb87dd82b397715ba194f1795ae903b28987
Markus Röman <markus.roman@tuni.fi>
2019-10-22 18:21:46 +0300

    Toimivat taulukko-operaatiot

--- student/08/arrays/array_operations.cpp
+++ student/08/arrays/array_operations.cpp
@@ -1,1 +1,37 @@
 #include "array_operations.hh"
+#include <iostream>
+int greatest_v1(int* itemptr, int size) {
+    int greatest = 0;
+    for (int i = 0 ; i < size ; ++i) {
+        if (*itemptr > greatest) {
+            greatest = *itemptr;
+        }
+        ++itemptr;
+    }
+    return greatest;
+}
+int greatest_v2(int* itemptr, int* endptr) {
+    int greatest = 0;
+    for ( ; itemptr < endptr ; ++itemptr) {
+        if (*itemptr > greatest) {
+            greatest = *itemptr;
+        }
+    }
+    return greatest;
+}
+void copy(int* itemptr, int* endptr, int* targetptr) {
+    for ( ; itemptr < endptr ; ++itemptr) {
+        *targetptr = *itemptr;
+        ++targetptr;
+    }
+}
+void reverse(int* leftptr, int* rightptr) {
+    --rightptr;
+    while ( leftptr != rightptr && (leftptr) + 1 != rightptr ) {
+        int tmp = *leftptr;
+        *leftptr = *rightptr;
+        *rightptr = tmp;
+        ++leftptr;
+        --rightptr;
+    }
+}
